e03b3a48115bec82f418872f6b112fefbb11010a
Feng Haosheng <47263888+LangostaFeliz@users.noreply.github.com>
2019-12-06 23:20:02 -0800

    arreglar el bug de la collision de basura periodico con el vagon y regenera jar

--- src/gameObjeto/basura/basuraPapel/BasuraPeriodico.java
+++ src/gameObjeto/basura/basuraPapel/BasuraPeriodico.java
@@ -11,7 +11,7 @@ package gameObjeto.basura.basuraPapel;
 
 import gameObjeto.basura.Basura;
 
-public class BasuraPeriodico extends Basura {
+public class BasuraPeriodico extends BasuraPapel {
     public BasuraPeriodico(double x, double y, double width, double height, double hitboxSize) {
         super(x, y, width, height, hitboxSize);
     }
